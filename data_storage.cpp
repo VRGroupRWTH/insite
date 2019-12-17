d79841a9ff67050a83c78171a8c07814d107e091
Simon <oehrl@vr.rwth-aachen.de>
2019-12-17 12:44:01 +0100

    Change simulation step to simulation time

--- data_storage.cpp
+++ data_storage.cpp
@@ -39,19 +39,21 @@ DataStorage::DataStorage(
   //                             spikes_data_space, spikes_set_properties_);
 }
 
-void DataStorage::AddSpike(std::uint64_t simulation_step, std::uint64_t gid) {
+void DataStorage::AddSpike(double simulation_time, std::uint64_t gid) {
   std::unique_lock<std::mutex> lock(spike_mutex_);
   constexpr auto spike_occured_before = [](const Spike& lhs, const Spike& rhs) {
-    return lhs.simulation_step < rhs.simulation_step;
+    return lhs.simulation_time < rhs.simulation_time;
   };
-  const Spike spike {simulation_step, gid};
-  const auto equal_range = std::equal_range(buffered_spikes_.begin(), buffered_spikes_.end(), spike, spike_occured_before);
+  const Spike spike{simulation_time, gid};
+  const auto equal_range =
+      std::equal_range(buffered_spikes_.begin(), buffered_spikes_.end(), spike,
+                       spike_occured_before);
   for (auto i = equal_range.first; i != equal_range.second; ++i) {
     if (i->gid == gid) {
       return;
     }
   }
-  buffered_spikes_.insert(equal_range.second,spike);
+  buffered_spikes_.insert(equal_range.second, spike);
 }
 
 std::vector<Spike> DataStorage::GetSpikes() {
