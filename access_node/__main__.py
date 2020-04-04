9a429c2862cc74b877f2890cfa060439e018a2b2
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2020-04-04 19:30:33 +0200

    Fix indentation

--- access_node/__main__.py
+++ access_node/__main__.py
@@ -13,7 +13,7 @@ import requests
 import psycopg2
 
 def ConnectToDatabase(postgres_username, postgres_password):
-	database_host = 'database'
+    database_host = 'database'
     with open('database_host.txt') as database_host_file:
         database_host = database_host_file.readline()
     return psycopg2.connect(database="postgres", user="postgres",
@@ -21,28 +21,28 @@ def ConnectToDatabase(postgres_username, postgres_password):
 
 
 def main():
-	# Wait for simulation nodes to post to database
-	time.sleep(5)
+    # Wait for simulation nodes to post to database
+    time.sleep(5)
 
-	# get simulation nodes
-	con = ConnectToDatabase('postgres', 'postgres')
-	cur = con.cursor()
-	# NEST
-	cur.execute("SELECT address FROM nest_simulation_node")
-	nodes.nest_simulation_nodes = [i[0] for i in cur.fetchall()]
-	# Arbor
-	cur.execute("SELECT address FROM nest_simulation_node")
-	nodes.arbor_simulation_nodes = [i[0] for i in cur.fetchall()]
-	con.close()
+    # get simulation nodes
+    con = ConnectToDatabase('postgres', 'postgres')
+    cur = con.cursor()
+    # NEST
+    cur.execute("SELECT address FROM nest_simulation_node")
+    nodes.nest_simulation_nodes = [i[0] for i in cur.fetchall()]
+    # Arbor
+    cur.execute("SELECT address FROM nest_simulation_node")
+    nodes.arbor_simulation_nodes = [i[0] for i in cur.fetchall()]
+    con.close()
 
 
-	# run acces_node
-	app = connexion.App(__name__, specification_dir='./swagger/')
-	app.app.json_encoder = encoder.JSONEncoder
-	app.add_api('swagger.yaml', arguments={
-				'title': 'In-Situ Pipeline REST API'})
-	CORS(app.app)
-	app.run(port=8080)
+    # run acces_node
+    app = connexion.App(__name__, specification_dir='./swagger/')
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('swagger.yaml', arguments={
+                'title': 'In-Situ Pipeline REST API'})
+    CORS(app.app)
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
