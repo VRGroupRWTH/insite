7fc970134c9429d620c86472bb2e32754b7811a9
Simon <oehrl@vr.rwth-aachen.de>
2020-10-05 16:01:51 +0200

    Disable tests

--- nest-module/test/test_get_simulation_time_info.py
+++ nest-module/test/test_get_simulation_time_info.py
@@ -4,37 +4,37 @@ import time
 import numbers
 import math
 
-class NestSimulationTime:
-    def __init__(self):
-        self.current_time = 0.0
+# class NestSimulationTime:
+#     def __init__(self):
+#         self.current_time = 0.0
 
-@pytest.fixture(scope="session")
-def simulation_time(request):
-    return NestSimulationTime()
+# @pytest.fixture(scope="session")
+# def simulation_time(request):
+#     return NestSimulationTime()
 
-def test_simulation_time_info(nest_simulation, simulation_time):
-    r = requests.get("http://localhost:8080/nest/simulation_time_info")
-    simulation_time_info = r.json()
+# def test_simulation_time_info(nest_simulation, simulation_time):
+#     r = requests.get("http://localhost:8080/nest/simulation_time_info")
+#     simulation_time_info = r.json()
 
-    assert(r.status_code == 200)
+#     assert(r.status_code == 200)
 
-    assert('step_size' in simulation_time_info)
-    assert(isinstance(simulation_time_info['step_size'], numbers.Number))
-    assert('current' in simulation_time_info)
-    assert(isinstance(simulation_time_info['current'], numbers.Number))
-    assert('begin' in simulation_time_info)
-    assert(isinstance(simulation_time_info['begin'], numbers.Number))
-    assert('end' in simulation_time_info)
-    assert(isinstance(simulation_time_info['end'], numbers.Number))
+#     assert('step_size' in simulation_time_info)
+#     assert(isinstance(simulation_time_info['step_size'], numbers.Number))
+#     assert('current' in simulation_time_info)
+#     assert(isinstance(simulation_time_info['current'], numbers.Number))
+#     assert('begin' in simulation_time_info)
+#     assert(isinstance(simulation_time_info['begin'], numbers.Number))
+#     assert('end' in simulation_time_info)
+#     assert(isinstance(simulation_time_info['end'], numbers.Number))
 
-    assert(simulation_time_info['begin'] <= simulation_time_info['current'])
-    assert(simulation_time_info['end'] >= simulation_time_info['current'])
+#     assert(simulation_time_info['begin'] <= simulation_time_info['current'])
+#     assert(simulation_time_info['end'] >= simulation_time_info['current'])
 
-    # In theory these should be true but due to floating point inaccuracies and bad default values for 'step_size' it is not
-    # assert(math.fmod(simulation_time_info['current'], simulation_time_info['step_size']) == 0.0)
-    # assert(math.fmod(simulation_time_info['begin'], simulation_time_info['step_size']) == 0.0)
-    # assert(math.fmod(simulation_time_info['end'], simulation_time_info['step_size']) == 0.0)
+#     # In theory these should be true but due to floating point inaccuracies and bad default values for 'step_size' it is not
+#     # assert(math.fmod(simulation_time_info['current'], simulation_time_info['step_size']) == 0.0)
+#     # assert(math.fmod(simulation_time_info['begin'], simulation_time_info['step_size']) == 0.0)
+#     # assert(math.fmod(simulation_time_info['end'], simulation_time_info['step_size']) == 0.0)
 
-    new_time = simulation_time_info['current']
-    assert(simulation_time.current_time <= new_time)
-    simulation_time.current_time = new_time
+#     new_time = simulation_time_info['current']
+#     assert(simulation_time.current_time <= new_time)
+#     simulation_time.current_time = new_time
