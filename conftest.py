f9878103ebe47d90de396d3c9c7126d4135b5a7b
Simon <oehrl@vr.rwth-aachen.de>
2020-10-16 11:59:32 +0200

    Use nest module to check version

--- conftest.py
+++ conftest.py
@@ -28,7 +28,7 @@ def nest_simulation(request):
     while True:
         time.sleep(1.0)
         try:
-            r = requests.get("http://localhost:8080/version")
+            r = requests.get("http://localhost:8000/version")
             if r.status_code == 200:
                 break
         except requests.exceptions.ConnectionError:
