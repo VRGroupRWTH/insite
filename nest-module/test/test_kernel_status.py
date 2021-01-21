c984fc03012ab67ba85d72dc42ab665f6fc6a0e3
Marcel Krüger <marcel.krueger@rwth-aachen.de>
2021-01-21 18:34:09 +0100

    Fix tests for get kernel status

--- nest-module/test/test_kernel_status.py
+++ nest-module/test/test_kernel_status.py
@@ -5,14 +5,13 @@ def test_get_kernel_status(nest_simulation):
     request = requests.get("http://localhost:9000/kernelStatus")
     assert(request.status_code == 200)
     
-    kernel_statuses = request.json()
-    assert(isinstance(kernel_statuses, list))
+    kernel_status = request.json()
 
-    for kernel_status in kernel_statuses:
-        assert(isinstance(kernel_status, dict))
 
-        assert('num_processes' in kernel_status)
-        assert(kernel_status['num_processes'] == 1)
+    assert(isinstance(kernel_status, dict))
+
+    assert('num_processes' in kernel_status)
+    assert(isinstance(kernel_status['num_processes'],numbers.Number))
         
-        assert('local_num_threads' in kernel_status)
-        assert(kernel_status['local_num_threads'] == 1)
+    assert('local_num_threads' in kernel_status)
+    assert(isinstance(kernel_status['local_num_threads'],numbers.Number))
