59ee5696c563b7fdf74968ac6c8feb37c8653978
Sebastian Spreizer <spreizer@web.de>
2021-04-09 16:48:11 +0200

    Rename spike detector to spike recorder in recording_backend

--- nest-module/src/recording_backend_insite.cpp
+++ nest-module/src/recording_backend_insite.cpp
@@ -11,7 +11,7 @@
 #include "vp_manager_impl.h"
 
 // Includes from topology:
-#include "topology.h"
+// #include "topology.h"
 
 // Includes from sli:
 #include "dictutils.h"
@@ -34,8 +34,8 @@ void RecordingBackendInsite::finalize() {
 
 void RecordingBackendInsite::enroll(const nest::RecordingDevice &device,
                                     const DictionaryDatum &params) {
-  if (device.get_type() == nest::RecordingDevice::SPIKE_DETECTOR) {
-    std::cout << "[insite] enroll spike detector (" << device.get_label() << ") with id " << device.get_node_id() << "\n";
+  if (device.get_type() == nest::RecordingDevice::SPIKE_RECORDER) {
+    std::cout << "[insite] enroll spike recorder (" << device.get_label() << ") with id " << device.get_node_id() << "\n";
     data_storage_.CreateSpikeDetectorStorage(device.get_node_id());
   } else if (device.get_type() == nest::RecordingDevice::MULTIMETER) {
     std::cout << "[insite] enroll multimeter (" << device.get_label() << ") with id " << device.get_node_id() << "\n";
@@ -92,7 +92,7 @@ void RecordingBackendInsite::write(const nest::RecordingDevice &device,
                                    const std::vector<long> &long_values) {
   const auto sender_gid = event.get_sender_node_id();
   const auto time_stamp = event.get_stamp().get_ms();
-  if (device.get_type() == nest::RecordingDevice::SPIKE_DETECTOR) {
+  if (device.get_type() == nest::RecordingDevice::SPIKE_RECORDER) {
     data_storage_.AddSpike(device.get_node_id(), time_stamp, sender_gid);
   } else if (device.get_type() == nest::RecordingDevice::MULTIMETER) {
     data_storage_.AddMultimeterMeasurement(device.get_node_id(), time_stamp, sender_gid, double_values, long_values);
