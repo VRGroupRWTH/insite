28a495f26d68ff522a0ffc2b1cbf68350d9260b0
Timon Römer <t.roemer@vr.rwth-aachen.de>
2021-12-10 16:00:28 +0100

    Rewrote comments and function-names and cleaned up test-code

--- access-node/test/nest_general_test_functions.py
+++ access-node/test/nest_general_test_functions.py
@@ -1,3 +1,4 @@
+import json
 import time
 import numbers
 import math
@@ -6,85 +7,81 @@ from operator import truediv
 import requests
 from requests.api import request
 from requests.sessions import Request
+from config import *
 
-#Server response for a succesfull operation
-HTTP_QUERY_SUCCESS_CODE = 200
-
-#Name that is used to access simulation times
-SIMULATION_TIMES_NAME = "simulationTimes"
-
-#Name that is used to access node IDs
-NODE_IDS_NAME = "nodeIds"
-
-#Checks if HTTP-request is valid, and returns spike-data in Json format.
-def nest_check_request_valid(request):
+#Checks if the given HTTP-request has a valid http-response-code. Returns the spike-data in Json format.
+def check_request_valid(request):
     request = requests.get(request)
-    assert(request.status_code == HTTP_QUERY_SUCCESS_CODE)
+    assert(request.status_code == requests.codes.ok)
     spikes = request.json()
     return spikes
 
-#Checks if the given spike data are in a valid format
+#Converts the given spike data to pairs out of nodeId and corresponding simulation times
+def zip_spikes(spikes):
+    return zip(spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value], spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value])
+
+#Checks if the given spike data has a correct length, is sorted by time and does not include values smaller than zero
 def spikes_check_if_valid_format(spikes):
     spikes_check_data_length_valid(spikes)
-    spikes_sorted_by_time(spikes)
-    spikes_check_if_nodeIds_have_minimum(spikes, 0)
-    spikes_check_if_simulation_times_have_minimum(spikes, 0)
+    spikes_check_if_sorted_by_time(spikes)
+    spikes_check_if_nodeIds_are_greater_or_equal_than(spikes, 0)
+    spikes_check_if_simulation_times_are_greater_or_equal_than(spikes, 0)
 
-#Checks if the given spike-data is sorted correctly by time
-def spikes_sorted_by_time(spikes):
+#Checks that every entry is greater or equal than his predecessor
+def spikes_check_if_sorted_by_time(spikes):
     previous_time = 0
-    for time in spikes[SIMULATION_TIMES_NAME]:
+    for time in spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]:
         assert(time >= previous_time)
         previous_time = time
 
 #Checks if nodeIds of the given spike data have the desired minimum
-def spikes_check_if_nodeIds_have_minimum(spikes, minimum):
-    for time in spikes[NODE_IDS_NAME]:
-        assert(time >= minimum)
+def spikes_check_if_nodeIds_are_greater_or_equal_than(spikes, minimum):
+    for id in spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]:
+        assert(id >= minimum)
 
-#Checks if the length of the given spike-data is valid
+#Checks if the length of the two given spike-data sets is equal and if exactly the two wanted data-sets are included
 def spikes_check_data_length_valid(spikes):
-    assert(NODE_IDS_NAME in spikes)
-    assert(SIMULATION_TIMES_NAME in spikes)
+    assert(JSON_VALUE_TO_FIELD_NAME.simulationTimes.value in spikes)
+    assert(JSON_VALUE_TO_FIELD_NAME.nodeIds.value in spikes)
     assert(len(spikes.keys()) == 2)
-    assert(len(spikes[NODE_IDS_NAME]) == len(spikes[SIMULATION_TIMES_NAME]))
-    assert(len(spikes[NODE_IDS_NAME]) > 0)
+    assert(len(spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]) == len(spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]))
+    assert(len(spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]) > 0)
 
-#Checks and returns whether the two given spike data is equal
-def spikes_check_if_equal_data(spike_data_a, spike_data_b):
-    assert(len(spike_data_a[SIMULATION_TIMES_NAME]) == len(spike_data_b[SIMULATION_TIMES_NAME]))
-    assert(len(spike_data_a[NODE_IDS_NAME]) == len(spike_data_b[NODE_IDS_NAME]))
+#Checks and returns whether the two given spike data-sets are equal
+def spikes_check_if_data_are_equal(spike_data_a, spike_data_b):
+    assert(len(spike_data_a[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]) == len(spike_data_b[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]))
+    assert(len(spike_data_a[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]) == len(spike_data_b[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]))
     
-    for i in range(len(spike_data_a[SIMULATION_TIMES_NAME])):
-        assert(spike_data_a[SIMULATION_TIMES_NAME][i] == spike_data_b[SIMULATION_TIMES_NAME][i])
-        assert(spike_data_a[NODE_IDS_NAME][i] == spike_data_b[NODE_IDS_NAME][i])
+    for i in range(len(spike_data_a[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value])):
+        assert(spike_data_a[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value][i] == spike_data_b[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value][i])
+        assert(spike_data_a[JSON_VALUE_TO_FIELD_NAME.nodeIds.value][i] == spike_data_b[JSON_VALUE_TO_FIELD_NAME.nodeIds.value][i])
 
 #Checks if the given simulation times are all greater than the desired time
-def spikes_check_if_simulation_times_have_minimum(spikes, minimum_time):
-    for time in spikes[SIMULATION_TIMES_NAME]:
+def spikes_check_if_simulation_times_are_greater_or_equal_than(spikes, minimum_time):
+    for time in spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]:
         assert(time >= minimum_time)
     
 #Checks if the given simulation times are all smaller than the desired time
-def spikes_check_if_simulation_times_have_maximum(spikes, maximum_time):
-    for time in spikes[SIMULATION_TIMES_NAME]:
+def spikes_check_if_simulation_times_are_smaller_or_equal_than(spikes, maximum_time):
+    for time in spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]:
         assert(time <= maximum_time)
 
 #Checks if the given spike-data only includes the desired nodeIDs
 def spikes_check_if_only_nodeIds_included(spikes, nodeId_list):
-    for id in spikes[NODE_IDS_NAME]:
+    for id in spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]:
         assert(id in nodeId_list)
 
-#Checks if the given skipped spikes have the desired offset to the given unskipped spikes
+#Checks if the given skipped spikes have the desired offset in comparison to the given unskipped spikes
 def spikes_check_if_has_offset(skipped_spikes, non_skipped_spikes, skip):
-    assert(len(skipped_spikes[NODE_IDS_NAME]) + skip == len(non_skipped_spikes[NODE_IDS_NAME]))
-    assert(len(skipped_spikes[SIMULATION_TIMES_NAME]) + skip == len(non_skipped_spikes[SIMULATION_TIMES_NAME]))
+    assert(len(skipped_spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]) + skip == len(non_skipped_spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]))
+    assert(len(skipped_spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]) + skip == len(non_skipped_spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]))
 
     for i in range(len(skipped_spikes)):
-        assert(non_skipped_spikes[NODE_IDS_NAME][skip + i] == skipped_spikes[NODE_IDS_NAME][i])
-        assert(non_skipped_spikes[SIMULATION_TIMES_NAME][skip + i] == skipped_spikes[SIMULATION_TIMES_NAME][i])    
+        assert(non_skipped_spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value][skip + i] == skipped_spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value][i])
+        assert(non_skipped_spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value][skip + i] == skipped_spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value][i])    
 
 #Checks if the given spike-data has the offset defined in parameters in comparison to the spike-data with the given parameters but without the offset
-def spikes_check_if_has_offset_with_parameters(REQUEST_URL, skipped_spikes, PARAMETER_NAME_LIST, nest_get_spikes_query_parameters, parameter_set_combination = [True, True, True, True, True]):
+def spikes_check_if_has_offset_to_request_with_parameters(REQUEST_URL, skipped_spikes, PARAMETER_NAME_LIST, nest_get_spikes_query_parameters, parameter_set_combination = [True, True, True, True, True]):
     parameter_set_combination = list(parameter_set_combination)
 
     if parameter_set_combination[4]:
@@ -92,116 +89,125 @@ def spikes_check_if_has_offset_with_parameters(REQUEST_URL, skipped_spikes, PARA
         parameter_set_combination[4] = False
     
         query_string_no_skip_no_top = build_query_string(REQUEST_URL, PARAMETER_NAME_LIST, nest_get_spikes_query_parameters, parameter_set_combination)
-        spikes_no_skip_no_top = nest_check_request_valid(query_string_no_skip_no_top)
+        spikes_no_skip_no_top = check_request_valid(query_string_no_skip_no_top)
         spikes_check_if_valid_format(spikes_no_skip_no_top)
         
         startIndex = nest_get_spikes_query_parameters[3]
         endIndex = nest_get_spikes_query_parameters[3] + nest_get_spikes_query_parameters[4]
 
-        spikes_no_skip_no_top[NODE_IDS_NAME] = spikes_no_skip_no_top[NODE_IDS_NAME][startIndex:endIndex]
-        spikes_no_skip_no_top[SIMULATION_TIMES_NAME] = spikes_no_skip_no_top[SIMULATION_TIMES_NAME][startIndex:endIndex]
+        spikes_no_skip_no_top[JSON_VALUE_TO_FIELD_NAME.nodeIds.value] = spikes_no_skip_no_top[JSON_VALUE_TO_FIELD_NAME.nodeIds.value][startIndex:endIndex]
+        spikes_no_skip_no_top[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value] = spikes_no_skip_no_top[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value][startIndex:endIndex]
 
         spikes_check_if_has_offset(skipped_spikes, spikes_no_skip_no_top, 0)
     else:
         parameter_set_combination[3] = False
         query_string_no_skip = build_query_string(REQUEST_URL, PARAMETER_NAME_LIST, nest_get_spikes_query_parameters, parameter_set_combination)
-        spikes_no_skip = nest_check_request_valid(query_string_no_skip)
+        spikes_no_skip = check_request_valid(query_string_no_skip)
         spikes_check_if_valid_format(spikes_no_skip)
 
         spikes_check_if_has_offset(skipped_spikes, spikes_no_skip, nest_get_spikes_query_parameters[3])
 
 #Checks if the given spike-data the desired amount of entries
-def spikes_check_if_number_less_or_equal_to(spikes, maximum_amount):
-    assert(len(spikes[NODE_IDS_NAME]) <= maximum_amount)
-    assert(len(spikes[SIMULATION_TIMES_NAME]) <= maximum_amount)
+def spikes_check_if_number_of_entries_less_or_equal_to(spikes, maximum_number):
+    assert(len(spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value]) <= maximum_number)
+    assert(len(spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value]) <= maximum_number)
 
 #Filters and returns the given spike data by deleting every entrie with a simulation time higher than the minimum time
-def filter_spike_data_with_minimum_time(spikes, minimum_time):
+def filter_for_spike_data_with_minimum_time(spikes, minimum_time):
     filtered_times = []
     filtered_nodes = []
 
-    for i in range(len(spikes[SIMULATION_TIMES_NAME])):
-        if spikes[SIMULATION_TIMES_NAME][i] >= minimum_time:
-            filtered_times.append(spikes[SIMULATION_TIMES_NAME][i])
-            filtered_nodes.append(spikes[NODE_IDS_NAME][i])
+    for spike_pair in zip_spikes(spikes):
+        node_id, time = spike_pair
+        if time >= minimum_time:
+            filtered_times.append(time)
+            filtered_nodes.append(node_id)
 
-    spikes[SIMULATION_TIMES_NAME] = filtered_times
-    spikes[NODE_IDS_NAME] = filtered_nodes
+    spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value] = filtered_times
+    spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value] = filtered_nodes
 
     return spikes
 
 #Filters and returns the given spike data by deleting every entry with a simulation time lower than the maximum time
-def filter_spike_data_with_maximum_time(spikes, maximum_time):
+def filter_for_spike_data_with_maximum_time(spikes, maximum_time):
     filtered_times = []
     filtered_nodes = []
 
-    for i in range(len(spikes[SIMULATION_TIMES_NAME])):
-        if spikes[SIMULATION_TIMES_NAME][i] <= maximum_time:
-            filtered_times.append(spikes[SIMULATION_TIMES_NAME][i])
-            filtered_nodes.append(spikes[NODE_IDS_NAME][i])
+    for spike_pair in zip_spikes(spikes):
+        node_id, time = spike_pair
+        if time <= maximum_time:
+            filtered_times.append(time)
+            filtered_nodes.append(node_id)
 
-    spikes[SIMULATION_TIMES_NAME] = filtered_times
-    spikes[NODE_IDS_NAME] = filtered_nodes
+    spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value] = filtered_times
+    spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value] = filtered_nodes
 
     return spikes
 
 #Filters and returns the given spike data by deleting every entry that does not belong to one of the given nodeIds
-def filter_spike_data_with_nodeIds(spikes, nodeIds):
+def filter_for_spike_data_with_nodeIds(spikes, nodeIds):
     filtered_times = []
     filtered_nodes = []
 
-    for i in range(len(spikes[SIMULATION_TIMES_NAME])):
-        if spikes[NODE_IDS_NAME][i] in nodeIds:
-            filtered_times.append(spikes[SIMULATION_TIMES_NAME][i])
-            filtered_nodes.append(spikes[NODE_IDS_NAME][i])
+    for spike_pair in zip_spikes(spikes):
+        node_id, time = spike_pair
+        if node_id in nodeIds:
+            filtered_times.append(time)
+            filtered_nodes.append(node_id)
 
-    spikes[SIMULATION_TIMES_NAME] = filtered_times
-    spikes[NODE_IDS_NAME] = filtered_nodes
+    spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value] = filtered_times
+    spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value] = filtered_nodes
 
     return spikes
 
-#Offsets the given spike data by the desired amount
-def filter_spike_data_with_offset(spikes, offset):
+#Offsets the given spike data by the desired amount and returns the result
+def offset_spike_data(spikes, offset):
     filtered_times = []
     filtered_nodes = []
 
-    for i in range(len(spikes[SIMULATION_TIMES_NAME])):
-        if i >= offset:
-            filtered_times.append(spikes[SIMULATION_TIMES_NAME][i])
-            filtered_nodes.append(spikes[NODE_IDS_NAME][i])
+    count = 0
+    for spike_pair in zip_spikes(spikes):
+        node_id, time = spike_pair
+        if count >= offset:
+            filtered_times.append(time)
+            filtered_nodes.append(node_id)
+        count = count + 1
 
-    spikes[SIMULATION_TIMES_NAME] = filtered_times
-    spikes[NODE_IDS_NAME] = filtered_nodes
+    spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value] = filtered_times
+    spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value] = filtered_nodes
 
     return spikes
 
 #Filters and returns the given spike data by only keeping the desired number of entries
-def filter_spike_data_with_number(spikes, max_number):
+def only_keep_top_spike_data(spikes, max_number):
     filtered_times = []
     filtered_nodes = []
 
-    for i in range(len(spikes[SIMULATION_TIMES_NAME])):
-        if i < max_number:
-            filtered_times.append(spikes[SIMULATION_TIMES_NAME][i])
-            filtered_nodes.append(spikes[NODE_IDS_NAME][i])
+    count = 0
+    for spike_pair in zip_spikes(spikes):
+        node_id, time = spike_pair
+        if count < max_number:
+            filtered_times.append(time)
+            filtered_nodes.append(node_id)
+        count = count + 1
 
-    spikes[SIMULATION_TIMES_NAME] = filtered_times
-    spikes[NODE_IDS_NAME] = filtered_nodes
+    spikes[JSON_VALUE_TO_FIELD_NAME.simulationTimes.value] = filtered_times
+    spikes[JSON_VALUE_TO_FIELD_NAME.nodeIds.value] = filtered_nodes
 
     return spikes
 
 #Applies the given parameters to the given, non filtered spike data and returns the filtered result
-def apply_nest_get_spikes_filters(spikes, parameter_values, parameter_set_list):
+def filter_spike_data_with_parameters_locally(spikes, parameter_values, parameter_set_list):
     if parameter_set_list[0]:
-        spikes = filter_spike_data_with_minimum_time(spikes, parameter_values[0])
+        spikes = filter_for_spike_data_with_minimum_time(spikes, parameter_values[0])
     if parameter_set_list[1]:
-        spikes = filter_spike_data_with_maximum_time(spikes, parameter_values[1])
+        spikes = filter_for_spike_data_with_maximum_time(spikes, parameter_values[1])
     if parameter_set_list[2]:
-        spikes = filter_spike_data_with_nodeIds(spikes, parameter_values[2])
+        spikes = filter_for_spike_data_with_nodeIds(spikes, parameter_values[2])
     if parameter_set_list[3]:
-        spikes = filter_spike_data_with_offset(spikes, parameter_values[3])
+        spikes = offset_spike_data(spikes, parameter_values[3])
     if parameter_set_list[4]:
-        spikes = filter_spike_data_with_number(spikes, parameter_values[4])
+        spikes = only_keep_top_spike_data(spikes, parameter_values[4])
 
     return spikes
 
@@ -214,13 +220,14 @@ def build_query_string(prefix_string, paramater_name_list, param_value_list, par
     query_string = prefix_string + "?"
     is_first_paramater = True
 
-    for i in range(len(paramater_is_set_list)):
+    i = 0
+    for param in paramater_name_list:
         if paramater_is_set_list[i]:
             if is_first_paramater:
-                query_string += paramater_name_list[i]
+                query_string += param.value
                 is_first_paramater = False
             else:
-                query_string += "&" + paramater_name_list[i]
+                query_string += "&" + param.value
 
             query_string += "="
 
@@ -235,7 +242,7 @@ def build_query_string(prefix_string, paramater_name_list, param_value_list, par
                         query_string += "," + str(value)
             else:
                 query_string += str(param_value_list[i])
-                
+        i = i + 1        
     return query_string
 
 #Tests every possible combination of query-parameters with a given query prefix and the given parameters by using the given handling functions
@@ -244,11 +251,11 @@ def check_all_parameter_combinations_with_conditions_and_values(REQUEST_URL, par
 
     for parameter_set_combination in combinations:
         query_string = build_query_string(REQUEST_URL, parameter_name_list, parameter_values, parameter_set_combination)
-        filtered_spikes = nest_check_request_valid(query_string)
+        filtered_spikes = check_request_valid(query_string)
         spikes_check_if_valid_format(filtered_spikes)
 
-        unfiltered_spikes = nest_check_request_valid(REQUEST_URL)
+        unfiltered_spikes = check_request_valid(REQUEST_URL)
         spikes_check_if_valid_format(unfiltered_spikes)
-        self_filtered_spikes = apply_nest_get_spikes_filters(unfiltered_spikes, parameter_values, parameter_set_combination)
+        self_filtered_spikes = filter_spike_data_with_parameters_locally(unfiltered_spikes, parameter_values, parameter_set_combination)
 
-        spikes_check_if_equal_data(filtered_spikes, self_filtered_spikes)
+        spikes_check_if_data_are_equal(filtered_spikes, self_filtered_spikes)
--- access-node/test/test_nest_get_spikes.py
+++ access-node/test/test_nest_get_spikes.py
@@ -9,84 +9,83 @@ from requests.sessions import Request
 from nest_general_test_functions import *
 
 #URL used for the "nest_get_spikes" HTTP-query
-URL_NEST_GET_SPIKES = "http://localhost:8080/nest/spikes"
+URL_NEST_GET_SPIKES = BASE_REQUEST_URL + "/spikes"
 
-#Names for the "nest_get_spikes" parameters
-NEST_GET_SPIKES_PARAMETER_NAME_LIST = [
-    "fromTime",
-    "toTime",
-    "nodeIds",
-    "skip",
-    "top"
-]
+#Names for the "nest_get_spikes" request-parameters
+class NEST_GET_SPIKES_PARAMETER_NAME_LIST (Enum):
+    fromTime = "fromTime"
+    toTime = "toTime"
+    nodeIds = "nodeIds"
+    skip = "skip"
+    top = "top"
 
 #Tests a default nest_get_spikes request withour any parameters
 def test_nest_get_spikes_no_parameters():
-    spikes_check_if_valid_format(nest_check_request_valid(URL_NEST_GET_SPIKES))
+    spikes_check_if_valid_format(check_request_valid(URL_NEST_GET_SPIKES))
 
-#Tests a nest_get_spikes request with the "fromTime" parameter
+#Tests a nest_get_spikes request with the "fromTime" parameter by checking if all returned times are greater or equal than requested
 def test_nest_get_spikes_parameter_fromTime(): 
     parameter_values = [15, 0, 0, 0, 0]
     parameter_set_combination = [True, False, False, False, False]
 
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
     spikes_check_if_valid_format(filtered_spikes)
 
-    spikes_check_if_simulation_times_have_minimum(filtered_spikes, parameter_values[0])
+    spikes_check_if_simulation_times_are_greater_or_equal_than(filtered_spikes, parameter_values[0])
 
-#Tests a nest_get_spikes request with the "toTime" parameter
+#Tests a nest_get_spikes request with the "toTime" parameter by checking if all returned times are smaller or equal than requested
 def test_nest_get_spikes_parameter_toTime(): 
     parameter_values = [0, 25, 0, 0, 0]
     parameter_set_combination = [False, True, False, False, False]
 
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
     spikes_check_if_valid_format(filtered_spikes)
 
-    spikes_check_if_simulation_times_have_maximum(filtered_spikes, parameter_values[1])
+    spikes_check_if_simulation_times_are_smaller_or_equal_than(filtered_spikes, parameter_values[1])
 
-#Tests a nest_get_spikes request with the "fromTime" and the "toTime" parameter
+#Tests a nest_get_spikes request with the "fromTime" and the "toTime" parameter by checking if all returned times are in thr requested timespan
 def test_nest_get_spikes_parameter_fromTime_toTime(): 
     parameter_values = [10, 25, 0, 0, 0]
     parameter_set_combination = [True, True, False, False, False]
 
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
     spikes_check_if_valid_format(filtered_spikes)
 
-    spikes_check_if_simulation_times_have_minimum(filtered_spikes, parameter_values[0])
-    spikes_check_if_simulation_times_have_maximum(filtered_spikes, parameter_values[1]) 
+    spikes_check_if_simulation_times_are_greater_or_equal_than(filtered_spikes, parameter_values[0])
+    spikes_check_if_simulation_times_are_smaller_or_equal_than(filtered_spikes, parameter_values[1]) 
 
-#Tests a nest_get_spikes request with the "nodeIds" parameter
+#Tests a nest_get_spikes request with the "nodeIds" parameter by checking if only the requested nodeIDs are returned
 def test_nest_get_spikes_parameter_nodeIds():
     node_ids = [11, 101]
     parameter_values = [0, 0, node_ids, 0, 0]
     parameter_set_combination = [False, False, True, False, False]
     
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
     spikes_check_if_valid_format(filtered_spikes)
 
     spikes_check_if_only_nodeIds_included(filtered_spikes, parameter_values[2])
 
-#Test a nest_get_spikes request with the "skip" parameter
+#Test a nest_get_spikes request with the "skip" parameter by checking if a request without the "skip" parameter returns the same result but offset
 def test_nest_get_spikes_parameter_skip():
     parameter_values = [0, 0, 0, 4, 0]
     parameter_set_combination = [False, False, False, True, False]
 
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
     spikes_check_if_valid_format(filtered_spikes)
 
-    spikes_check_if_has_offset_with_parameters(URL_NEST_GET_SPIKES, filtered_spikes, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination)
+    spikes_check_if_has_offset_to_request_with_parameters(URL_NEST_GET_SPIKES, filtered_spikes, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination)
 
-#Test a nest_get_spikes request with the "top" parameter
+#Test a nest_get_spikes request with the "top" parameter by checking if the number of entries is smaller or equal than the desired amount
 def test_nest_get_spikes_parameter_top():
     parameter_values = [0, 0, 0, 0, 23]
     parameter_set_combination = [False, False, False, False, True]
 
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, parameter_values, parameter_set_combination))
     spikes_check_if_valid_format(filtered_spikes)
 
-    spikes_check_if_number_less_or_equal_to(filtered_spikes, parameter_values[4])
+    spikes_check_if_number_of_entries_less_or_equal_to(filtered_spikes, parameter_values[4])
 
-#Tests a nest_get_spikes request qith all possible parameters
+#Tests a nest_get_spikes request with the paramaters: "fromTime", "toTime", "nodeIds", "skip" and "top" by checking if the conditions for each of the parameters apply to the returned data
 def test_nest_get_spikes_all_parameters():
     paramater_values = [
         2,
@@ -96,16 +95,16 @@ def test_nest_get_spikes_all_parameters():
         20000
     ]
     
-    filtered_spikes = nest_check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, paramater_values))
+    filtered_spikes = check_request_valid(build_query_string(URL_NEST_GET_SPIKES, NEST_GET_SPIKES_PARAMETER_NAME_LIST, paramater_values))
     spikes_check_if_valid_format(filtered_spikes)
 
-    spikes_check_if_simulation_times_have_minimum(filtered_spikes, paramater_values[0])
-    spikes_check_if_simulation_times_have_maximum(filtered_spikes, paramater_values[1])
+    spikes_check_if_simulation_times_are_greater_or_equal_than(filtered_spikes, paramater_values[0])
+    spikes_check_if_simulation_times_are_smaller_or_equal_than(filtered_spikes, paramater_values[1])
     spikes_check_if_only_nodeIds_included(filtered_spikes, paramater_values[2])
-    spikes_check_if_has_offset_with_parameters(URL_NEST_GET_SPIKES, filtered_spikes, NEST_GET_SPIKES_PARAMETER_NAME_LIST, paramater_values)
-    spikes_check_if_number_less_or_equal_to(filtered_spikes, paramater_values[4])
+    spikes_check_if_has_offset_to_request_with_parameters(URL_NEST_GET_SPIKES, filtered_spikes, NEST_GET_SPIKES_PARAMETER_NAME_LIST, paramater_values)
+    spikes_check_if_number_of_entries_less_or_equal_to(filtered_spikes, paramater_values[4])
 
-#Tests every possible combination of the nest_get_spikes query-parameters
+#Tests every possible combination of the nest_get_spikes query-parameters "fromTime", "toTime", "nodeIds", "skip" and "top" by checking if the conditions for each of the parameters apply to the returned data
 def test_nest_get_spikes_parameter_combinations():
     paramater_values = [
         2,
