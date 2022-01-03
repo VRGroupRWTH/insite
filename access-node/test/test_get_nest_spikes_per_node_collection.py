6f5d6ef42c6122b0c2bd04129a1b613b7436fda7
Timon Römer <t.roemer@vr.rwth-aachen.de>
2022-01-03 16:54:31 +0100

    Improvedf and refactored NEST_GET_SPIKES_PER_NODE_COLLECTION Test

--- access-node/test/test_get_nest_spikes_per_node_collection.py
+++ access-node/test/test_get_nest_spikes_per_node_collection.py
@@ -1,33 +1,42 @@
-import requests
+import time
 import numbers
+import math
+import itertools
+from operator import truediv
+import requests
+from requests.api import request
+from requests.sessions import Request
+from nest_general_test_functions import *
 
+#URL used for the "nest_get_spikes_per_node_collection" HTTP-query
+URL_NEST_GET_SPIKES_PER_NODE_COLLECTION = BASE_REQUEST_URL + "/nodeCollections"
+
+#Names for the "nest_get_spikes_per_node_collection" property names
+class NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES:
+    nodeCollectionId = "nodeCollectionId"
+    toTime = "toTime"
+    nodeIds = "nodeIds"
+    nodes = "nodes"
+    lastId = "lastId"
+    firstId = "firstId"
 
 def test_get_nest_spikes_per_node_collection(nest_simulation):
-    r = requests.get("http://localhost:8080/nest/nodeCollections")
-    assert(r.status_code == 200)
+    node_collections = check_request_valid(URL_NEST_GET_SPIKES_PER_NODE_COLLECTION)
 
-    node_collections = r.json()
     collection_ids = []
 
     for collection in node_collections:
-      collection_ids.append(collection["nodeCollectionId"])
+      collection_ids.append(collection[NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.nodeCollectionId])
     
-    r = requests.get("http://localhost:8080/nest/nodeCollections")
-    assert(r.status_code == 200)
-    node_collections = r.json()
-
-
     for collection_id in collection_ids:
-      url = "http://localhost:8080/nest/nodeCollections/{0}/spikes".format(collection_id)
-      r = requests.get(url)
-      assert(r.status_code == 200)
-      spikes = r.json()
+      url = URL_NEST_GET_SPIKES_PER_NODE_COLLECTION + "/{0}/spikes".format(collection_id)
+      spikes = check_request_valid(url)
 
-      current_collection = next(item for item in node_collections if item['nodeCollectionId'] == collection_id)
+      current_collection = next(item for item in node_collections if item[NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.nodeCollectionId] == collection_id)
 
-      for node_id in spikes['nodeIds']:
-        assert(node_id <= current_collection['nodes']['lastId'])
-        assert(node_id >= current_collection['nodes']['firstId'])
+      for node_id in spikes[NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.nodeIds]:
+        assert(node_id <= current_collection[NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.nodes][NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.lastId])
+        assert(node_id >= current_collection[NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.nodes][NEST_GET_SPIKES_PER_NODE_COLLECTION_PROPERTY_NAMES.firstId])
 
      
   
