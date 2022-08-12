f823c1d6762a7dbfbde9369449e0b0ff4c091c7b
Marcel Krueger <krueger@vr.rwth-aachen.de>
2022-08-12 18:38:09 +0200

    squash me

--- nest-module/src/http_server.hpp
+++ nest-module/src/http_server.hpp
@@ -1,10 +1,13 @@
 #ifndef HTTP_SERVER_HPP
 #define HTTP_SERVER_HPP
 
-#include <cpprest/http_listener.h>
+// #include <cpprest/http_listener.h>
+#include <future>
 #include <string>
 #include <memory>
 #include <regex>
+#include "crow/app.h"
+#include "crow/http_request.h"
 
 namespace insite {
 
@@ -12,32 +15,37 @@ class DataStorage;
 
 class HttpServer {
  public:
-  HttpServer(web::http::uri address, DataStorage* storage);
+  // HttpServer(web::http::uri address, DataStorage* storage);
+  HttpServer(std::string address, DataStorage* storage);
   void SimulationHasEnded(double end_time);
   void ClearSimulationHasEnded();
  private:
-  web::http::experimental::listener::http_listener http_listener_;
+  // web::http::experimental::listener::http_listener http_listener_;
+  crow::SimpleApp app;
+  std::future<void> crow_server;
   DataStorage* storage_;
   double simulation_has_ended_ = -1;  
-  web::http::http_response GetVersion(const web::http::http_request& request);
-  web::http::http_response GetKernelStatus(const web::http::http_request& request);
-  web::http::http_response GetCollections(const web::http::http_request& request);
-  web::http::http_response GetNodes(const web::http::http_request& request);
-  web::http::http_response GetSpikeDetectors(const web::http::http_request& request);
-  web::http::http_response GetSpikeRecorders(const web::http::http_request& request);
-  web::http::http_response GetMultimeters(const web::http::http_request& request);
+  // web::http::http_response GetVersion(const web::http::http_request& request);
+  // web::http::http_response GetKernelStatus(const web::http::http_request& request);
+  // web::http::http_response GetCollections(const web::http::http_request& request);
+  // web::http::http_response GetNodes(const web::http::http_request& request);
+  // web::http::http_response GetSpikeDetectors(const web::http::http_request& request);
+  // web::http::http_response GetSpikeRecorders(const web::http::http_request& request);
+  // web::http::http_response GetMultimeters(const web::http::http_request& request);
+  //
+  // web::http::http_response GetCurrentSimulationTime(const web::http::http_request& request);
+  // web::http::http_response GetSpikes(const web::http::http_request& request);
+  crow::response GetSpikes(const crow::request& request);
+  crow::response GetSpikesFB(const crow::request& request);
+  // web::http::http_response GetMultimeterMeasurement(const web::http::http_request& request);
 
-  web::http::http_response GetCurrentSimulationTime(const web::http::http_request& request);
-  web::http::http_response GetSpikes(const web::http::http_request& request);
-  web::http::http_response GetMultimeterMeasurement(const web::http::http_request& request);
-
-  struct Error {
-    std::string code;
-    std::string message;
-
-    web::json::value Serialize() const;
-  };
-  static web::http::http_response CreateErrorResponse(web::http::status_code status_code, const Error& error);
+  // struct Error {
+  //   std::string code;
+  //   std::string message;
+  //
+  //   web::json::value Serialize() const;
+  // };
+  // static web::http::http_response CreateErrorResponse(web::http::status_code status_code, const Error& error);
   std::vector<std::uint64_t> CommaListToUintVector(std::string input, std::regex regex = std::regex("((\\%2C|,)+)"));
 };
 
