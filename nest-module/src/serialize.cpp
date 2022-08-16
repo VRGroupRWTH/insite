c30adb2730cfe468c0bd41796f7c407e0e5d88b8
Marcel Krueger <krueger@vr.rwth-aachen.de>
2022-08-16 18:04:14 +0200

    refactor(nest-module): add rapidjson serializer for datums

--- nest-module/src/serialize.cpp
+++ nest-module/src/serialize.cpp
@@ -1,25 +1,64 @@
 #include "serialize.hpp"
 
-#include "stringdatum.h"
-#include "booldatum.h"
-#include "integerdatum.h"
-#include "doubledatum.h"
-#include "namedatum.h"
-#include "arraydatum.h"
-#include "dictdatum.h"
-#include <cmath>
+#include <rapidjson/document.h>
+#include <rapidjson/rapidjson.h>
 
+#include <cmath>
+#include <string>
+
+#include "arraydatum.h"
+#include "booldatum.h"
+#include "dictdatum.h"
+#include "doubledatum.h"
+#include "integerdatum.h"
+#include "namedatum.h"
+#include "rapidjson/document.h"
+#include "spdlog/spdlog.h"
+#include "stringdatum.h"
 
 namespace insite {
+
+void SerializeDatum(Datum* datum, rapidjson::Writer<rapidjson::StringBuffer>& writer) {
+  if (IntegerDatum* integer_datum = dynamic_cast<IntegerDatum*>(datum)) {
+    writer.Int(*integer_datum);
+  } else if (DoubleDatum* double_datum = dynamic_cast<DoubleDatum*>(datum)) {
+    if (std::isfinite(*double_datum)) {
+      writer.Double(*double_datum);
+    } else {
+      writer.Null();
+    }
+  } else if (StringDatum* string_datum = dynamic_cast<StringDatum*>(datum)) {
+    writer.String(string_datum->c_str());
+  } else if (NameDatum* name_datum = dynamic_cast<NameDatum*>(datum)) {
+    writer.String(name_datum->toString().c_str());
+  } else if (BoolDatum* bool_datum = dynamic_cast<BoolDatum*>(datum)) {
+    writer.Bool(*bool_datum);
+  } else if (ArrayDatum* array_datum = dynamic_cast<ArrayDatum*>(datum)) {
+    writer.StartArray();
+    for (const auto& datum : *array_datum) {
+      SerializeDatum(datum.datum(), writer);
+    }
+    writer.EndArray();
+  } else if (DictionaryDatum* dictionary_datum = dynamic_cast<DictionaryDatum*>(datum)) {
+    writer.StartObject();
+    for (const auto& datum : **dictionary_datum) {
+      writer.Key(datum.first.toString().c_str());
+      SerializeDatum(datum.second.datum(), writer);
+    }
+    writer.EndObject();
+  } else {
+    writer.Null();
+  }
+}
 
 web::json::value SerializeDatum(Datum* datum) {
   if (IntegerDatum* integer_datum = dynamic_cast<IntegerDatum*>(datum)) {
     return web::json::value::number(*integer_datum);
   } else if (DoubleDatum* double_datum = dynamic_cast<DoubleDatum*>(datum)) {
-    if(std::isfinite(*double_datum)) {
-        return web::json::value::number(*double_datum);
-    }else{
-        return web::json::value::Null;
+    if (std::isfinite(*double_datum)) {
+      return web::json::value::number(*double_datum);
+    } else {
+      return web::json::value::Null;
     }
   } else if (StringDatum* string_datum = dynamic_cast<StringDatum*>(datum)) {
     return web::json::value::string(*string_datum);
