be1be4a83f7c5aeee80d70b0648618ff493e7986
Marcel Krueger <krueger@vr.rwth-aachen.de>
2023-01-31 09:39:47 +0100

    test(all): change test dir structure

---
+++ test/access_node/test_nest_get_simulation_time_info.py
@@ -0,0 +1,52 @@
+
+import requests
+import pytest
+import time
+import numbers
+import math
+from nest_general_test_functions import *
+
+class NestSimulationTime:
+    def __init__(self):
+        self.current_time = 0.0
+        self.ticked = False
+@pytest.fixture(scope="session")
+def simulation_time(request):
+    sim_time = NestSimulationTime()
+    yield sim_time
+    #if request.config.option.count > 1:
+    #    assert(sim_time.ticked)
+
+@pytest.mark.repeat(1)
+def test_simulation_time_info_fields(nest_simulation, simulation_time):
+     
+    simulation_time_info = return_json_body_if_status_ok(BASE_REQUEST_URL + '/simulationTimeInfo')
+
+    assert('stepSize' in simulation_time_info)
+    assert(isinstance(simulation_time_info['stepSize'], numbers.Number))
+    assert('current' in simulation_time_info)
+    assert(isinstance(simulation_time_info['current'], numbers.Number))
+    assert('begin' in simulation_time_info)
+    assert(isinstance(simulation_time_info['begin'], numbers.Number))
+    assert('end' in simulation_time_info)
+    assert(isinstance(simulation_time_info['end'], numbers.Number))
+    
+    assert(simulation_time_info['begin'] >= 0)
+    assert(simulation_time_info['end'] >= 0)
+    assert(simulation_time_info['current'] >= 0)
+    assert(simulation_time_info['begin'] <= simulation_time_info['current'])
+    assert(simulation_time_info['end'] >= simulation_time_info['current'])
+
+def test_simulation_time_info_time(nest_simulation, simulation_time):
+    simulation_time_info = return_json_body_if_status_ok(BASE_REQUEST_URL + '/simulationTimeInfo')
+    new_time = simulation_time_info['current']
+    assert(simulation_time.current_time <= new_time)
+
+    if(simulation_time.current_time != new_time):
+        simulation_time.ticked=True
+    simulation_time.current_time = new_time
+
+@pytest.mark.order(after="test_order.py::test_sim_finished")
+def test_simulation_time_info_endtime():
+    simulation_time_info = return_json_body_if_status_ok(BASE_REQUEST_URL + '/simulationTimeInfo')
+    assert(simulation_time_info['current']==simulation_time_info['end'])
---
+++ test/nest_module/test_get_multimeters.py
@@ -0,0 +1,26 @@
+import requests
+import numbers
+
+def test_get_multimeters(nest_simulation):
+    request = requests.get("http://localhost:18080/multimeters")
+    assert(request.status_code == 200)
+    
+    multimeters = request.json()
+
+    assert(isinstance(multimeters, list))
+
+    for multimeter in multimeters:
+        assert('multimeterId' in multimeter)
+        assert(isinstance(multimeter['multimeterId'], int))
+
+        assert('attributes' in multimeter)
+        assert(isinstance(multimeter['attributes'], list))
+
+        for attribute in multimeter['attributes']:
+            assert(isinstance(attribute, str))
+
+        assert('nodeIds' in multimeter)
+        assert(isinstance(multimeter['nodeIds'], list))
+
+        for node_id in multimeter['nodeIds']:
+            assert(isinstance(node_id, int))
