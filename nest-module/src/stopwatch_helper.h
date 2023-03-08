7adc58ea831fdeb1bc789e5d3862837673f12a09
Marcel Krueger <krueger@vr.rwth-aachen.de>
2023-03-08 13:35:40 +0100

    refactor(nest-module): split stopwatch helper into header and body

--- nest-module/src/stopwatch_helper.h
+++ nest-module/src/stopwatch_helper.h
@@ -1,27 +1,19 @@
+#pragma once
+#include <spdlog/fmt/chrono.h>
+#include <spdlog/fmt/fmt.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/stopwatch.h>
 #include <chrono>
 #include <utility>
 #include <vector>
-#include "spdlog/spdlog.h"
-#include "spdlog/stopwatch.h"
 
 namespace insite {
 struct StopwatchHelper {
   std::vector<std::pair<std::chrono::duration<double>, std::string>> checkpoints;
-  void checkpoint(const std::string& checkpoint_name) {
-    checkpoints.push_back(std::make_pair(sw.elapsed(), checkpoint_name));
-    sw.reset();
-  }
 
-  void reset() {
-    sw.reset();
-  }
-
-  void print() {
-    spdlog::info("Checkpoints: ");
-    for (const auto& checkpoint : checkpoints) {
-      spdlog::info("{} - {}", checkpoint.first, checkpoint.second);
-    }
-  }
+  void checkpoint(const std::string& checkpoint_name);
+  void reset();
+  void print();
   spdlog::stopwatch sw{};
 };
 }  // namespace insite
