85ab446e14696c1f37700d460ce05e9ed43caee8
Marcel Krueger <krueger@vr.rwth-aachen.de>
2023-03-31 09:24:13 +0200

    test(arbor-tests): fix spike tests

--- test/access_node/arbor/arbor_config.py
+++ test/access_node/arbor/arbor_config.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 #Base URL and prefix for every HTTP-querie to the Arbor-Server
-BASE_REQUEST_URL = "http://localhost:52056/arbor"
+BASE_REQUEST_URL = "http://insite-access-node:52056/arbor"
 
 #Relates every value to its corresponding name in the spike-data JSON-field
 class JSON_VALUE_TO_FIELD_NAME(Enum):
     simulationTimes = "simulationTimes"
-    nodeIds = "nodeIds"
+    nodeIds = "gIds"
     spikerecorderId = "spikerecorderId"
     spikerecorders = "spikerecorders"
     spikes = "spikes"
