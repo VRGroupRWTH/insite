835818698e9ac73852700db352c8ab13e2dfb9fe
Marcel Krueger <krueger@vr.rwth-aachen.de>
2023-03-31 08:03:51 +0200

    test: add dockerized pytests

--- test/conftest.py
+++ test/conftest.py
@@ -14,40 +14,50 @@ def nest_simulation(request):
     # subprocess.run(["docker-compose", "build"], stdout=logfile, stderr=subprocess.STDOUT)
     print("Finished docker-compose build")
     print("Starting docker-compose")
-    # process = subprocess.Popen(["docker-compose", "-f", "docker-compose-testing.yml" ,"up"], stdout=logfile, stderr=subprocess.STDOUT)
-    process = subprocess.Popen(["python","./nest-module/example/pytest_simulation.py"], stdout=logfile, stderr=subprocess.STDOUT)
-    process2 = subprocess.Popen(["./insite-access-node"], stdout=logfile_access_node, stderr=subprocess.STDOUT)
+    # process = subprocess.Popen(["docker-compose", "-f", "insite.yml" ,"up"], stdout=logfile, stderr=subprocess.STDOUT)
+    # process = subprocess.Popen(["docker-compose","./nest-module/example/pytest_simulation.py"], stdout=logfile, stderr=subprocess.STDOUT)
+
+
+    # process2 = subprocess.Popen(["./insite-access-node"], stdout=logfile_access_node, stderr=subprocess.STDOUT)
     time.sleep(1)
+    print("\nWaiting for NEST start up",end='')
 
     def finalize():
         # process.send_signal(signal.SIGINT)
-        process.send_signal(signal.SIGINT)
-        process2.send_signal(signal.SIGINT)
+        # process.send_signal(signal.SIGINT)
+        # process2.send_signal(signal.SIGINT)
         # os.kill(process.pid, signal.SIGINT)
-        # os.kill(process2.pid, signal.SIGINT)
-        try:
-            process.wait(10)
-        except subprocess.TimeoutExpired:
-            print("\nSending SIGKILL to docker-compose")
+        # # os.kill(process2.pid, signal.SIGINT)
+        # subprocess.run(["docker-compose", "-f", "insite.yml" ,"down"], stdout=logfile, stderr=subprocess.STDOUT)
+        # try:
+        #     # process.wait(10)
+        # except subprocess.TimeoutExpired:
+        #     print("\nSending SIGKILL to docker-compose")
             # os.kill(process.pid, signal.SIGKILL)
             # os.kill(process2.pid, signal.SIGKILL)
-            process.kill()
-            process2.kill()
-            process.wait()
-            process2.wait()
+            # process.kill()
+            # process2.kill()
+            # process.wait()
+            # process2.wait()
         # subprocess.run(["docker-compose", "down"], stdout=logfile, stderr=subprocess.STDOUT)
         logfile.close()
+    print("\nWaiting for NEST start up",end='')
     request.addfinalizer(finalize)
 
     begin_time = time.time()
-    print("\nWaiting for insite start up",end='')
+    print("\nWaiting for NEST start up",end='')
     while True:
         print(".",end='',flush=True)
         time.sleep(1.0)
         try:
-            r = requests.get("http://localhost:18080/version")
+            r = requests.get("http://insite-nest-module:52425/")
+            print("\nNEST started",end='')
+
             if r.status_code == 200:
                 print("started.")
+                print("sending nest sim")
+                process = subprocess.Popen(["python", "/tests/nest_test_network/pytest_simulation_server.py"], stdout=logfile, stderr=subprocess.STDOUT)
+
                 break
         except requests.exceptions.ConnectionError:
             pass
@@ -60,7 +70,7 @@ def nest_simulation(request):
         print(".",end='',flush=True)
         time.sleep(1.0)
         try:
-            r = requests.get("http://localhost:52056/version")
+            r = requests.get("http://insite-access-node:52056/version")
             if r.status_code == 200:
                 print("started.")
                 break
